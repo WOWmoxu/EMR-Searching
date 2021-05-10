aa3b39ba32a3ab48ad998697a42dbdfa79893afb
WOWmoxu <1048550477@qq.com>
2021-05-10 17:33:11 +0800

    zc上传

--- filter_condition_query/invoke_query.py
+++ filter_condition_query/invoke_query.py
@@ -31,50 +31,53 @@ def query_all(all_conditions):
     :return:
     """
     # 拼接SQL
-    head_sql = "MATCH "
-    tail_sql = "WHERE "
-
+    sql_str = "MATCH {} WHERE {} RETURN m_r.id"
+    sql_part = ""
+    sql_part2 = ""
     # 遍历条件字典
     for key, value in all_conditions.items():
         if key == '姓名':
-            sql_part = 'm_r.name="' + value[0] + '"'
+            sql_part += 'm_r.name="' + value[0] + '" AND '
         elif key == '性别':
-            sql_part = '(m_r:medical_record)-[:肯定关系]->(xb:cxbmc),'
-            sql_part2 = 'xb.name="' + value[0] + '" AND'
+            xb_list = []
+            for content in value:
+                xb_list += content
+            sql_part += '(m_r:medical_record)-[:肯定关系]->(xb:cxbmc),'
+            sql_part2 += 'xb.name In %s AND '%xb_list
         elif key == '年龄':
             for i, content in enumerate(value):
-                sql_part2 = 'm_r.age_year' + content[0] + " AND"
+                sql_part2 += 'm_r.age_year' + content[0] + " AND "
         elif key == '否定症状':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:否定关系]->(sys%s:symptom_node),' % i
-                sql_part2 = 'sys%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:否定关系]->(not_sys%s:symptom_node),' % i
+                sql_part2 += 'not_sys%s.name="' % i + content[0] + '" AND '
         elif key == '否定疾病':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:否定关系]->(dis%s:disease_node),' % i
-                sql_part2 = 'dis%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:否定关系]->(not_dis%s:disease_node),' % i
+                sql_part2 += 'not_dis%s.name="' % i + content[0] + '" AND '
         elif key == '否定手术治疗':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:否定关系]->(o_t:operation_treatment_node),' % i
-                sql_part2 = 'o_t%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:否定关系]->(not_o_t:operation_treatment_node),' % i
+                sql_part2 += 'not_o_t%s.name="' % i + content[0] + '" AND '
         elif key == '否定非手术治疗':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:否定关系]->(n_o_t:not_operation_treatment_node),' % i
-                sql_part2 = 'n_o_t%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:否定关系]->(n_o_t:not_operation_treatment_node),' % i
+                sql_part2 += 'n_o_t%s.name="' % i + content[0] + '" AND '
         elif key == '否定药品':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:否定关系]->(drug%s:drug_node),' % i
-                sql_part2 = 'drug%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:否定关系]->(not_drug%s:drug_node),' % i
+                sql_part2 += 'not_drug%s.name="' % i + content[0] + '" AND '
         elif key == '科室':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[肯定关系]->(ks:cksmc),' % i
-                sql_part2 = 'ks%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[肯定关系]->(ks:cksmc),' % i
+                sql_part2 += 'ks%s.name="' % i + content[0] + '" AND '
         elif key == '部位对应症状':
             for i, content in enumerate(value):
-                for j, child_content in enumerate(content):
-                    sql_part = '(m_r:medical_record)-[肯定关系]->(b_p%s:body_parts_node)-[:肯定关系]->(sys%s:symptom_node)' % (
-                    j, j)
-                    sql_part2 = 'b_p.name = "' + child_content[0] + '" AND ' + 'sys.name IN {}'.format(
-                        child_content[1:])
+                # for j, child_content in enumerate(content):
+                sql_part += '(m_r:medical_record)-[肯定关系]->(b_p%s:body_parts_node)-[:肯定关系]->(b_p_sys%s:symptom_node),' % (
+                    i, i)
+                sql_part2 += 'b_p%s.name = "'%i + content[0] + '" AND ' + 'b_p_sys{}.name IN {}'.format(i,
+                    content[1:]) + ' AND '
         elif key == '精神特征对应结果':
             # *********
             pass
@@ -83,24 +86,30 @@ def query_all(all_conditions):
             pass
         elif key == '生理指标对应数值':
             # *********
-            pass
+            for i, content in enumerate(value):
+
+                pass
         elif key == '肯定症状':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:肯定关系]->(dis%s:disease_node),' % i
-                sql_part2 = 'dis%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:肯定关系]->(dis%s:disease_node),' % i
+                sql_part2 += 'dis%s.name="' % i + content[0] + '" AND '
         elif key == '肯定手术治疗':
             # ************
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:肯定关系]->(o_t:operation_treatment_node),' % i
-                sql_part2 = 'o_t%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:肯定关系]->(o_t:operation_treatment_node),' % i
+                sql_part2 += 'o_t%s.name="' % i + content[0] + '" AND '
         elif key == '肯定非手术治疗':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:肯定关系]->(n_o_t:not_operation_treatment_node),' % i
-                sql_part2 = 'n_o_t%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:肯定关系]->(y_n_o_t%s:not_operation_treatment_node),' % i
+                sql_part2 += 'y_n_o_t%s.name="' % i + content[0] + '" AND '
         elif key == '肯定药品':
             for i, content in enumerate(value):
-                sql_part = '(m_r:medical_record)-[:肯定关系]->(drug%s:drug_node),' % i
-                sql_part2 = 'drug%s.name="' % i + content[0] + '" AND'
+                sql_part += '(m_r:medical_record)-[:肯定关系]->(drug%s:drug_node),' % i
+                sql_part2 += 'drug%s.name="' % i + content[0] + '" AND '
+
+    final = sql_str.format(sql_part[:-1], sql_part2[:-4])
+    print(final)
+    return final
 
 
 def query_sc(sufficient_condition):
